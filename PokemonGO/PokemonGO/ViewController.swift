ac9c21ba91e8d336384e00f79622fe5cddbdb241
Marcos Koch <marcos_koch@hotmail.com>
2018-11-13 17:22:57 -0200

    feat: Location Permission

--- PokemonGO/PokemonGO/ViewController.swift
+++ PokemonGO/PokemonGO/ViewController.swift
@@ -1,18 +1,27 @@
 //
 //  ViewController.swift
 //  PokemonGO
-//
+//  
 //  Created by Marcos Koch on 13/11/18.
 //  Copyright © 2018 Marcos Koch. All rights reserved.
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
 
+    @IBOutlet weak var mapa: MKMapView!
+    var gerenciadorLocalizacao = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        mapa.delegate = self
+        gerenciadorLocalizacao.delegate = self
+        gerenciadorLocalizacao.requestWhenInUseAuthorization()
+        gerenciadorLocalizacao.startUpdatingLocation()
+        
     }
 
 
