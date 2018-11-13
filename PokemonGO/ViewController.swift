5826f0357503b5fa84422826b0bf14f8b1af68dd
Marcos Koch <marcos_koch@hotmail.com>
2018-11-13 16:21:58 -0200

    Add Project

---
+++ PokemonGO/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PokemonGO
+//
+//  Created by Marcos Koch on 13/11/18.
+//  Copyright © 2018 Marcos Koch. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
